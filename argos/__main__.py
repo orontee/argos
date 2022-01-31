198d55d83a709c3586cb1a9ada179f1f1307b0bd
Matthias Meulien <orontee@gmail.com>
2022-01-31 13:24:00 +0100

    Remove dead code
    Signed-off-by: Matthias Meulien <orontee@gmail.com>

--- argos/__main__.py
+++ argos/__main__.py
@@ -3,25 +3,9 @@
 """Mopidy UI for argos.
 
 """
-
-import argparse
-import logging
 import sys
 
 from .app import Application
-
-LOGGER = logging.getLogger("argosui")
-
-
-def configure_logger(args: argparse.Namespace) -> None:
-    ch = logging.StreamHandler()
-    formatter = logging.Formatter("%(levelname)s: %(message)s")
-    ch.setFormatter(formatter)
-    level = logging.DEBUG if args.debug is True else logging.INFO
-    ch.setLevel(level)
-    logger = logging.getLogger("argosui")
-    logger.setLevel(level)
-    logger.addHandler(ch)
 
 
 if __name__ == "__main__":
