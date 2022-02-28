4d4eb789d269272a6dc53340ae7b574758042600
Matthias Meulien <orontee@gmail.com>
2022-02-28 19:25:51 +0100

    Improve readability
    Signed-off-by: Matthias Meulien <orontee@gmail.com>

--- argos/accessor.py
+++ argos/accessor.py
@@ -15,6 +15,14 @@ class ModelAccessor:
         self._message_queue = message_queue
         self._changed: Set[str] = set()
 
+    def _set_model_attr(self, name, value) -> None:
+        if not hasattr(self._model, name):
+            LOGGER.warning(f"Attempt to set unexpected attribute {name}")
+
+        if getattr(self._model, name) != value:
+            setattr(self._model, name, value)
+            self._changed.add(name)
+
     async def __aenter__(self) -> "ModelAccessor":
         return self
 
@@ -29,33 +37,13 @@ class ModelAccessor:
         return False
 
     def clear_tl(self) -> None:
-        if self._model.track_uri:
-            self._model.track_uri = None
-            self._changed.add("track_uri")
-
-        if self._model.track_name:
-            self._model.track_name = None
-            self._changed.add("track_name")
-
-        if self._model.track_length:
-            self._model.track_length = None
-            self._changed.add("track_length")
-
-        if self._model.time_position:
-            self._model.time_position = 0
-            self._changed.add("time_position")
-
-        if self._model.artist_uri:
-            self._model.artist_uri = None
-            self._changed.add("artist_uri")
-
-        if self._model.artist_name:
-            self._model.artist_name = None
-            self._changed.add("artist_name")
-
-        if self._model.image_path:
-            self._model.image_path = None
-            self._changed.add("image_path")
+        self._set_model_attr("track_uri", None)
+        self._set_model_attr("track_name", None)
+        self._set_model_attr("track_length", None)
+        self._set_model_attr("time_position", None)
+        self._set_model_attr("artist_uri", None)
+        self._set_model_attr("artist_name", None)
+        self._set_model_attr("image_path", None)
 
     def update_from(
         self,
@@ -70,9 +58,7 @@ class ModelAccessor:
     ) -> None:
         if connected is not None:
             connected = True if connected is True else False
-            if self._model.connected != connected:
-                self._model.connected = connected
-                self._changed.add("connected")
+            self._set_model_attr("connected", connected)
 
         if raw_state is not None:
             try:
@@ -81,22 +67,16 @@ class ModelAccessor:
                 LOGGER.error(f"Unexpected state {raw_state!r}")
                 state = None
 
-            if self._model.state != state:
-                self._model.state = state
-                self._changed.add("state")
+            self._set_model_attr("state", state)
 
         props = {"mute": mute, "volume": volume}
         for prop_name in props:
             value = props[prop_name]
             if value is not None:
-                if getattr(self._model, prop_name) != value:
-                    setattr(self._model, prop_name, value)
-                    self._changed.add(prop_name)
+                self._set_model_attr(prop_name, value)
 
         if time_position is not None:
-            if self._model.time_position != time_position:
-                self._model.time_position = time_position
-                self._changed.add("time_position")
+            self._set_model_attr("time_position", time_position)
 
         if tl_track is not None:
             # TODO check dict
@@ -105,32 +85,21 @@ class ModelAccessor:
             track_uri = track.get("uri")
             track_name = track.get("name")
             track_length = track.get("length")
-            if self._model.track_uri != track_uri:
-                self._model.track_uri = track_uri
-                self._model.track_name = track_name
-                self._model.track_length = track_length
-                self._model.image_path = None
-                self._changed |= set(
-                    [
-                        "track_uri",
-                        "track_name",
-                        "track_length",
-                        "time_position",
-                        "image_path",
-                    ]
-                )
+
+            self._set_model_attr("track_uri", track_uri)
+            self._set_model_attr("track_name", track_name)
+            self._set_model_attr("track_length", track_length)
+            self._set_model_attr("time_position", None)
+            self._set_model_attr("image_path", None)
 
             artists = track.get("artists", [{}])
             artist = artists[0]
             artist_uri = artist.get("uri")
             artist_name = artist.get("name")
-            if self._model.artist_uri != artist_uri:
-                self._model.artist_uri = artist_uri
-                self._model.artist_name = artist_name
-                self._changed |= set(["artist_uri", "artist_name"])
+
+            self._set_model_attr("artist_uri", artist_uri)
+            self._set_model_attr("artist_name", artist_name)
 
         if image_path is not None:
             path = Path(image_path)
-            if self._model.image_path != path:
-                self._model.image_path = path
-                self._changed.add("image_path")
+            self._set_model_attr("image_path", path)
